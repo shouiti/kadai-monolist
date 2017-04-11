12a4e01cf9c1b74db13f91ca92eff2949bd56bfa
shouiti <shouiti0401@outlook.jp>
2017-04-11 09:44:38 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,17 +11,23 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
     end
     
     redirect_back(fallback_location: root_path)
   end
-
+  
   def destroy
     @item = Item.find(params[:item_id])
     
     if params[:type] == 'Want'
       current_user.unwant(@item)
       flash[:success] = '商品の Want を解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item)
+      flash[:success] = '商品の Have を解除しました。'
     end
     
     redirect_back(fallback_location: root_path)
