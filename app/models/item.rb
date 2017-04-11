5db30d6a050c66fcebb0407d2c51c96671c7a079
shouiti <shouiti0401@outlook.jp>
2017-04-11 12:14:15 +0000

    ranking

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,5 +11,5 @@ class Item < ApplicationRecord
   has_many :want_users, through: :wants, class_name: 'User', source: :user
   
   has_many :haves, class_name: 'Have'
-  has_many :want_users, through: :wants, class_name: 'User', source: :user
+  has_many :have_users, through: :haves, class_name: 'User', source: :user
 end
